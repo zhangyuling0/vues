1cbcac53fc4d3f222dbf36b1449807d9ae2c8b5c
zhangyuling0 <17887606693@163.com>
2018-08-06 16:41:55 +0800

    two

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import routes from './routerConfig.js';
-
-Vue.use(Router)
-
-export default new Router({
-  routes
-})
--- src/store/modules/index.js
+++
@@ -1,6 +0,0 @@
-import todolist from './todolist';
-import shopcar from './shopcar';
-import zhoukao from './zhoukao';
-export default {
-    todolist,shopcar,zhoukao
-}
